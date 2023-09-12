e0ed5790671f75ab85c8e756fdba82fef7ec764a
sebachoh <sebaruiz01@gmail.com>
2023-09-12 14:45:03 -0500

    Updated FixedUpdate Movement and added the risizing to tiny with a timer
    -Movement moved to FixedUpdate
    -Now Resizing
    -Added a timer so we can't keep pressing the G key to resize all the time, now it's 3 seconds delay to be able to resize again. (I'd like to have it smoothier).

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float xMov, zMov;
-    [SerializeField] private float speed = 1.0f;
-    [SerializeField] private Rigidbody rb;
+    // Variables
+    private float xMov, zMov;
+    [SerializeField] private float speed = 5.0f;
+    private float delay = 3.0f;
+
+    private float nextTime;
+    private float currentTime;
+    // Components
+    private Rigidbody rb;
+    private Transform tr;
+    // Vectors
+    private Vector3 tinyScale = new(0.5f, 0.5f, 0.5f);
+    private Vector3 originalScale;
+    // Bools
+    [SerializeField] private bool isTiny = false;
+
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        tr = GetComponent<Transform>();
+        originalScale = transform.localScale;
+        Debug.Log(transform.localScale);
     }
 
     private void Update()
     {
-        Movement();
+        currentTime += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.G) && currentTime >= nextTime)
+        {
+            ChangingLocalScale();
+            nextTime = currentTime + delay;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();        
     }
 
     void Movement()
@@ -23,6 +52,19 @@ public class PlayerMovement : MonoBehaviour
         xMov = Input.GetAxisRaw("Horizontal");
         zMov = Input.GetAxisRaw("Vertical");
         rb.velocity = new Vector3(xMov * speed, rb.velocity.y, zMov * speed);
+    }
+    void ChangingLocalScale()
+    {
+        if(isTiny == false)
+        {
+            tr.localScale = tinyScale;
+            isTiny = true;
+        }
+        else
+        {
+            tr.localScale = originalScale;
+            isTiny = false;
+        }
     }
 }
     
