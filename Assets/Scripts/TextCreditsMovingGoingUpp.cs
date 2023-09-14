972650078e93b4d7720b6c0359f63241290f4449
sebachoh <sebaruiz01@gmail.com>
2023-09-14 15:06:36 -0500

    WINSCENE COMPLETED
    thiis is too much, winscene completed, all completed, added so many things I can't even remember

--- Assets/Scripts/TextCreditsMovingGoingUpp.cs
+++ Assets/Scripts/TextCreditsMovingGoingUpp.cs
@@ -6,15 +6,28 @@ public class TextCreditsMovingGoingUpp : MonoBehaviour
 {
     [SerializeField] float speed = 5.0f;
     public bool isWinningTextDestroyed = false;
+    Animator animatorDown;
+    Animator animatorUp;
+    Animator buttonAnim;
 
     void Start()
     {
-
+        animatorDown = GameObject.Find("MainTitleDown").GetComponent<Animator>();
+        animatorUp = GameObject.Find("MainTitleUp").GetComponent<Animator>();
+        buttonAnim = GameObject.Find("StartAgainButton").GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if(isWinningTextDestroyed == true) transform.Translate(Vector3.up * speed * Time.deltaTime);
+        if (transform.position.y >= 680.0f) Destroy(this.gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        animatorDown.SetTrigger("CreditsDestroyed");
+        animatorUp.SetTrigger("CreditsDestroyed");
+        buttonAnim.SetTrigger("CreditsDestroyed");
     }
 }
